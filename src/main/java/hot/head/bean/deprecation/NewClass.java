bb3926481eb4e2f5896cfcda8d7aaa577d18d31c
lokost39 <lokost39@yandex.ru>
2019-03-11 13:55:04 +0300

    DeprecatedClass

--- src/main/java/hot/head/bean/deprecation/NewClass.java
+++ src/main/java/hot/head/bean/deprecation/NewClass.java
@@ -1,8 +1,14 @@
 package hot.head.bean.deprecation;
 
-public class NewClass extends OldClass implements DeprecatedClass{
+public class NewClass /*extends OldClass*/ implements DeprecatedClass{
+
     @Override
     public void test1() {
         System.out.println("test 3");
+    }
+
+    @Override
+    public void test2() {
+        System.out.println("test 4");
     }
 }
--- src/main/java/hot/head/bean/deprecation/OldClass.java
+++ src/main/java/hot/head/bean/deprecation/OldClass.java
@@ -1,11 +1,20 @@
 package hot.head.bean.deprecation;
 
 import hot.head.annotation.DeprecatedClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-@DeprecatedClass(newImpl = NewClass.class)
+//@DeprecatedClass(newImpl = NewClass.class)
 public class OldClass implements hot.head.bean.deprecation.DeprecatedClass {
+    private OldClass() {
+    }
+
+    @Bean
+    public OldClass getOldClass(){
+        return new OldClass();
+    }
+
     @Override
     public void test1(){
         System.out.println("test 1");
